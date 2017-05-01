08bcf47d052dfb982e014b6f59ec62a31f19257a
Sean Beck <spcbeck@gmail.com>
2017-04-30 22:42:02 -0400

    Adding home, header, and footer template files.

---
+++ template-home.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Template Name: Boston DSA Home
+ *
+ * Template for displaying the Boston DSA Homepage.
+ *
+ * @package understrap
+ */
+
+get_header();
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+
+<div class="wrapper" id="full-width-page-wrapper">
+
+	<div class="<?php echo esc_html( $container ); ?>" id="content">
+
+		<div class="row">
+
+			<div class="col-md-12 content-area" id="primary">
+
+				<main class="site-main" id="main" role="main">
+					<h1>Home</h1>
+					<?php while ( have_posts() ) : the_post(); ?>
+
+						<?php get_template_part( 'loop-templates/content', 'page' ); ?>
+
+					<?php endwhile; // end of the loop. ?>
+
+				</main><!-- #main -->
+
+			</div><!-- #primary -->
+
+		</div><!-- .row end -->
+
+	</div><!-- Container end -->
+
+</div><!-- Wrapper end -->
+
+<?php get_footer(); ?>
