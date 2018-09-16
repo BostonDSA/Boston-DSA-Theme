f00808e00529a63ee0c4885b76a75f0ba8a715b9
Sean Beck <spcbeck@gmail.com>
2018-09-15 23:52:10 -0400

    Adding the PEWG sign up form too footer.

--- footer.php
+++ footer.php
@@ -11,7 +11,23 @@ $the_theme = wp_get_theme();
 $container = get_theme_mod( 'understrap_container_type' );
 ?>
 
+<!-- Begin MailChimp Signup Form -->
+<link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
+<div id="mc_embed_signup">
+	<form action="https://bostondsa.us17.list-manage.com/subscribe/post?u=f7ccb68e6f65b7f64c09b76f7&amp;id=c13b7543ee" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+	    <div id="mc_embed_signup_scroll">
+		<h2><label for="mce-EMAIL">Socialism in Your Inbox</label></h2>
+		<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+	    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+	    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_f7ccb68e6f65b7f64c09b76f7_c13b7543ee" tabindex="-1" value=""></div>
+	    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+	    </div>
+	</form>
+</div>
+
 <?php get_sidebar( 'footerfull' ); ?>
+
+<!--End mc_embed_signup-->
 
 <div class="wrapper" id="wrapper-footer">
 
@@ -22,6 +38,20 @@ $container = get_theme_mod( 'understrap_container_type' );
 			<div class="col-md-12">
 
 				<footer class="site-footer" id="colophon">
+					<div class="policy-menu pull-left">
+						<?php wp_nav_menu(
+							array(
+								'menu' 			  => 'policies',
+								'theme_location'  => 'primary',
+								'container_class' => '',
+								'container_id'    => 'footerNavbarNavDropdown',
+								'menu_class'      => 'navbar-nav footer-nav',
+								'fallback_cb'     => '',
+								'menu_id'         => 'main-menu',
+								'walker'          => new WP_Bootstrap_Navwalker(),
+							)
+						); ?>
+					</div>
 					<div class="social-media-links clearfix">
 						<div class="social-media-links-inner">
 							<h3>Follow Us</h3>
@@ -30,7 +60,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 						</div>
 					</div>
 					<div class="site-info">
-						<p class="copyright">© 2017 Boston Democratic Socialists of America.</p>
+						<p class="copyright">© 2018 Boston Democratic Socialists of America.</p>
 						<?php wp_nav_menu(
 							array(
 								'theme_location'  => 'primary',
